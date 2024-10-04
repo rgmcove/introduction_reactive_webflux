06cd8bf3afbc0b49aeeeac7b16e08b8aa28f1dc9
rago.mos <ralberto115@gmail.com>
2024-10-04 14:58:29 -0500

    chore: adicion de nuevo proyecto

---
+++ pruebas-servicios-step-verifier/src/test/java/com/step/verifier/PruebasServiciosStepVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.step.verifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasServiciosStepVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
