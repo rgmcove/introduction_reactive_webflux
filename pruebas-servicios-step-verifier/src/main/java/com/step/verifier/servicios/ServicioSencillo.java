0408c42b263ac055eeb082896e30cc7000e48b83
rago.mos <ralberto115@gmail.com>
2024-10-04 15:22:24 -0500

    feat: servicio con metodos mono y flux

---
+++ pruebas-servicios-step-verifier/src/main/java/com/step/verifier/servicios/ServicioSencillo.java
@@ -0,0 +1,24 @@
+package com.step.verifier.servicios;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Service
+public class ServicioSencillo {
+
+    public Mono<String> buscarUno() {
+        return Mono.just("hola");
+    }
+
+    public Flux<String> buscarTodos() {
+        return Flux.just("hola", "que", "tal", "estas");
+    }
+
+    public Flux<String> buscarTodosLentos() {
+        return Flux.just("hola", "que", "tal", "estas")
+                .delaySequence(Duration.ofSeconds(10));
+    }
+}
