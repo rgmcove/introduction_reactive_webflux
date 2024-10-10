7cd166248c3b8c8604fa885a3e17b426089e1e35
rago.mos <ralberto115@gmail.com>
2024-10-10 12:11:17 -0500

    Revert "feat: rest api reactiva con anotaciones"
    This reverts commit 90f21305aaac25406c9cd379c132e108e964871e.

---
+++ api-rest-reactiva-anotaciones/src/main/java/com/api/rest/reactiva/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.api.rest.reactiva.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/mono")
+    public Mono<String> getMono() {
+        return Mono.just("Bienvenidos a mi Rest API");
+    }
+
+    @GetMapping(value = "/flux", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<String> getFlux() {
+        return Flux.just("Bienvenidos ", "a ", "mi ", "Rest ", "API ")
+                .delayElements(Duration.ofSeconds(1)).log();
+    }
+
+
+}
