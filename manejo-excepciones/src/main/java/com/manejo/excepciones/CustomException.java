3fae9676c08023fc81c83cf2d9d2a26019e4b9db
rago.mos <ralberto115@gmail.com>
2024-10-10 12:12:38 -0500

    Revert "feat: ejemplos de manejo de excepciones"
    This reverts commit dbc8d158b192affc0c2d5b0052955fc1bc65c4c7.

--- manejo-excepciones/src/main/java/com/manejo/excepciones/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.manejo.excepciones;
-
-public class CustomException extends Exception {
-    public CustomException(String message, Throwable exception) {
-        super(message, exception);
-    }
-}
